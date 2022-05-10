c1c236185c27f78eac86a15780a9b4e69c01c950
Lennart Pegel <github@justlep.net>
2022-05-11 00:40:35 +0200

    beta.1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-export {connect} from 'lib/nedbclient.js';
-export {getClientInstance as getClient} from 'lib/client.js';
-export {Document} from 'lib/document.js';
-export {EmbeddedDocument} from 'lib/embedded-document.js';
+export {connect} from './lib/nedbclient.js';
+export {getClientInstance as getClient} from './lib/client.js';
+export {Document} from './lib/document.js';
+export {EmbeddedDocument} from './lib/embedded-document.js';
